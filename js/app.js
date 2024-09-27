43c2d25429f833dc9970ed583b90f2be1dd805e5
Saidburxon <saidburxon@gmail.com>
2024-09-27 18:42:11 +0500

    Js boyicha 13dan tashqari xammasi tog'ri qilindi

---
+++ js/app.js
@@ -0,0 +1,275 @@
+//Musbat yoki manfiy sonni aniqlash
+// let number;
+// number = +prompt("Musbat yoki manfiy sonni aniqlash");
+// if (number > 0) {
+//   console.log("Musbat son");
+// } else {
+//   console.log("Manfiy son");
+// }
+
+//Sonning juft yoki toq ekanligini aniqlash
+// let usernumber2;
+// usernumber2 = +prompt("Sonning juft yoki toq ekanligini aniqlash");
+// if (usernumber2 % 2) {
+//   console.log("Siz kiritgan son toq");
+// } else {
+//   console.log("siz kiritgan son juft");
+// }
+
+//Ikki sonni taqqoslash
+// let usernumber3;
+// let secondNumber;
+// usernumber3 = +prompt("Ikki sonni taqqoslash:Birinchi sonni kiriting");
+// secondNumber = +prompt("Ikki sonni taqqoslash:Ikkinchi sonni kiriting");
+// if (usernumber3 > secondNumber) {
+//   console.log("Katta");
+// }
+// if (usernumber3 == secondNumber) {
+//   console.log("Teng");
+// }
+// if (usernumber3 < secondNumber) {
+//   console.log("Kichik");
+// }
+
+//To'rtburchakning kvadrat ekanligini aniqlash
+// let usernumber4;
+// let secondNumber4;
+// usernumber4 = +prompt("Ikki sonni taqqoslash:Uzunligni kiriting");
+// secondNumber4 = +prompt("Ikki sonni taqqoslash:Kenglikni kiriting");
+// if (usernumber4 > secondNumber4) {
+//   console.log("Kvadrat emas");
+// }
+// if (usernumber4 == secondNumber4) {
+//   console.log("Kvadrat");
+// }
+// if (usernumber4 < secondNumber4) {
+//   console.log("Kvadrat emas");
+// }
+
+//Haroratni baholash
+// let temperature;
+// temperature = +prompt("Haroratni baholash");
+// if (temperature > 0) {
+//   console.log("Yozgi");
+// }
+// if (temperature < 0) {
+//   console.log("Qishki");
+// }
+//Baho chiqarish
+// let grade;
+// grade = +prompt("Baho chiqarish:O'quvchining bahosini kiriting");
+// if (grade == 5) {
+//   console.log("A'lo");
+// }
+// if (grade == 4) {
+//   console.log("Yaxshi");
+// }
+// if (grade == 3) {
+//   console.log("Qoniqarli");
+// }
+// if (grade == 2) {
+//   console.log("Yomon");
+// }
+// if (grade == 1) {
+//   console.log("Juda yomon");
+// }
+
+//Maksimal sonni aniqlash
+// let firstNumberFromUser = prompt("Birinchi sonni kiriting:");
+// let secondNumberFromUser = prompt("Ikkinchi sonni kiriting:");
+// let thirdNumberFromUser = prompt("Uchinchi sonni kiriting:");
+// let maksimal = Math.max(
+//   firstNumberFromUser,
+//   secondNumberFromUser,
+//   thirdNumberFromUser
+// );
+// console.log("Eng katta son: " + maksimal);
+// alert("Eng katta son: " + maksimal);
+//Besh xonali sonmi yoki yo'q
+// let son = prompt("Son kiriting:");
+// if (son.length === 5) {
+//   alert("Besh xonali");
+// } else {
+//   alert("Besh xonali emas");
+// }
+
+//Yil faslini aniqlash
+// let season;
+// season = +prompt("Yil faslini aniqlash:Faslni kiriting");
+// if (season == 1) {
+//   console.log("Qish");
+// }
+// if (season == 2) {
+//   console.log("Qish");
+// }
+// if (season == 3) {
+//   console.log("Bahor");
+// }
+// if (season == 4) {
+//   console.log("Bahor");
+// }
+// if (season == 5) {
+//   console.log("Bahor");
+// }
+// if (season == 6) {
+//   console.log("Yoz");
+// }
+// if (season == 7) {
+//   console.log("Yoz");
+// }
+// if (season == 8) {
+//   console.log("Yoz");
+// }
+// if (season == 9) {
+//   console.log("Kuz");
+// }
+// if (season == 10) {
+//   console.log("Kuz");
+// }
+// if (season == 11) {
+//   console.log("Kuz");
+// }
+// if (season == 12) {
+//   console.log("Qish");
+// }
+//else {
+//}
+
+//Ishchi kun yoki dam olish kuni
+// let day;
+// day = +prompt("Ishchi kun yoki dam olish kuni:Hafta kunini kiriting");
+// if (day == 1) {
+//   console.log("Ish kun");
+// }
+// if (day == 2) {
+//   console.log("Ish kuni");
+// }
+// if (day == 3) {
+//   console.log("Ish kuni");
+// }
+// if (day == 4) {
+//   console.log("Ish kuni");
+// }
+// if (day == 5) {
+//   console.log("Ish kuni");
+// }
+// if (day == 6) {
+//   console.log("Dam olish kuni");
+// }
+// if (day == 7) {
+//   console.log("Dam olish kuni");
+// } else {
+//   console.log("Biz yer sayyorasidamiz!");
+// }
+
+//Foydalanuvchining yoshlik holatini aniqlash
+// let birth;
+// birth = +prompt("Nechanchi yili tug'ilgansiz?");
+// if (birth < 2005) {
+//   console.log("Katta odam");
+// }
+// if (birth > 2005) {
+//   console.log("Yosh bola");
+// }
+
+// To'liq yoki qisman bog'liq soat
+// let time;
+// time = +prompt("Soatni kiriting");
+// if (time == 0) {
+//   console.log("Dam olish vaqti");
+// }
+// if (time == 1) {
+//   console.log("Dam olish vaqti");
+// }
+// if (time == 2) {
+//   console.log("Dam olish vaqti");
+// }
+// if (time == 3) {
+//   console.log("Dam olish vaqti");
+// }
+// if (time == 4) {
+//   console.log("Dam olish vaqti");
+// }
+// if (time == 5) {
+//   console.log("Dam olish vaqti");
+// }
+// if (time == 6) {
+//   console.log("Dam olish vaqti");
+// }
+// if (time == 7) {
+//   console.log("Dam olish vaqti");
+// }
+// if (time == 8) {
+//   console.log("Dam olish vaqti");
+// }
+// if (time == 9) {
+//   console.log("Ish vaqti");
+// }
+// if (time == 10) {
+//   console.log("Ish vaqti");
+// }
+// if (time == 11) {
+//   console.log("Ish vaqti");
+// }
+// if (time == 12) {
+//   console.log("Ish vaqti");
+// }
+// if (time == 13) {
+//   console.log("Ish vaqti");
+// }
+// if (time == 14) {
+//   console.log("Ish vaqti");
+// }
+// if (time == 15) {
+//   console.log("Ish vaqti");
+// }
+// if (time == 16) {
+//   console.log("Ish vaqti");
+// }
+// if (time == 17) {
+//   console.log("Ish vaqti");
+// }
+// if (time == 18) {
+//   console.log("Ish vaqti");
+// }
+// if (time == 19) {
+//   console.log("Dam olish vaqti");
+// }
+// if (time == 20) {
+//   console.log("Dam olish vaqti");
+// }
+// if (time == 21) {
+//   console.log("Dam olish vaqti");
+// }
+// if (time == 22) {
+//   console.log("Dam olish vaqti");
+// }
+// if (time == 23) {
+//   console.log("Dam olish vaqti");
+// }
+// if (time == 24) {
+//   console.log("Dam olish vaqti");
+// }
+//Tekin kirish
+// let birthYear;
+// birthYear = +prompt("Necha yoshda ekanligingizni kiriting!");
+// if (birthYear < 18) {
+//   console.log("Kirish tekin");
+// } else {
+//   console.log("Kirish to'lov talab qilinadi");
+// }
+
+//Ko'p sonli katta yoki kichik son
+// let numberOfUser1;
+// let numberOfUser2;
+// let numberOfUser3;
+// numberOfUser1 = +prompt("Birinchi sonni kiriting!");
+// numberOfUser2 = +prompt("Ikkinchi sonni kiriting!");
+// numberOfUser3 = +prompt("Uchinchi sonni kiriting!");
+// let maksimal = Math.max(numberOfUser1, numberOfUser2, numberOfUser3);
+// if (maksimal < 100) {
+//   console.log("Kichik son");
+// } else {
+//   console.log("Katta son");
+// }
+// console.log("Siz kiritgan eng katta son:" + maksimal);
